09fd2ae005add989b397b481d9536510b2547dc5
Sadie Wilhelm <sadiewilhelm@SADIE-WILHELM.local>
2015-06-11 19:17:20 -0700

    Updating requirements.txt for deployment.

--- model.py
+++ model.py
@@ -184,7 +184,9 @@ class Similarity(db.Model):
 
 def connect_to_db(app):
     """Connect the database to our Flask app."""
-
+    # DATABASE_URL = os.environ.get("DATABASE_URL",
+    #                           'postgresql://localhost/pensive_passport')
+    # app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/pensive_passport'
     # app.config['SQLALCHEMY_ECHO'] = True
     db.app = app
